3a9c613721b717e92d7b3946b62076b1d44bab41
Alina Mackenzie <alimknz@gmail.com>
2015-06-25 21:08:38 -0500

    Change to Druplicamper.

---
+++ druplicamper.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Bot::BasicBot::Pluggable;
+
+my $bot = Bot::BasicBot::Pluggable->new(
+  server => 'irc.freenode.net',
+  channels => [ '#midcamp','#alimac'],
+  nick => 'Druplicamper',
+  username => 'Druplicamper',
+  altnicks => ["Drupli_camper", "Drupli__camper"],
+  name => "Super-awesome pluggable bot for Drupal camp channels.",
+);
+
+$bot->load('Auth');
+#$bot->load('Loader');
+$bot->load("Seen");
+$bot->load('Karma');
+$bot->load('Infobot');
+
+$bot->run();
+
